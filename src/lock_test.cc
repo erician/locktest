9458cb871f47d2f3dd23e50039ade036d8f17d59
liuchaojie <zkliuchaojie@163.com>
2019-06-30 10:42:25 +0800

    print the runtime

--- src/lock_test.cc
+++ src/lock_test.cc
@@ -16,11 +16,13 @@
 
 #include <thread>
 #include <iostream>
+#include <sys/time.h>
 
 #define MAX_NR_THREADS      4
 #define NR_ADD_OPERATIONS   1000*1000*100
 
 volatile unsigned long counter = 0;
+double *times;
 
 void Add(locktest::AbstractLock *lock, int nr_ops) {
     for(int i=0; i<nr_ops; i++) {
@@ -34,25 +36,36 @@ void PrintResult(locktest::AbstractLock *lock, int nr_thread) {
     std::cout << lock->GetName();
     if(NR_ADD_OPERATIONS/nr_thread*nr_thread != counter) {
         std::cout << ", error, the counter is: " << counter << \
-        ", but expected: " << NR_ADD_OPERATIONS/nr_thread*nr_thread << \
-        ", thread num: " << nr_thread << std::endl;
+        ", but expected: " << NR_ADD_OPERATIONS/nr_thread*nr_thread;
         exit(-1);
     }else {
-        std::cout << ", ok" << \
-        ", thread num: " << nr_thread << std::endl;
+        std::cout << ", ok";
     }
+    std::cout << ", thread num: " << nr_thread << \
+    ", total time: " << times[1]-times[0] << \
+    ", the number of add operations: " << NR_ADD_OPERATIONS << std::endl;
+}
+
+double mysecond() {
+    struct timeval tp;
+    int i;
+    i = gettimeofday(&tp, NULL);
+    return ( (double)tp.tv_sec + (double)tp.tv_usec*1.e-6);
 }
 
 int main() {
     std::thread *threads[MAX_NR_THREADS];
+    times = new double [2];
     //test cpp mutex lock
     locktest::CppMutexLock *cpp_mutex_lock = new locktest::CppMutexLock();;
     for(int i=1; i<=MAX_NR_THREADS; i++) {
         counter = 0;
+        times[0] = mysecond();
         for(int j=0; j<i; j++)
             threads[j] = new std::thread(Add, cpp_mutex_lock, NR_ADD_OPERATIONS/i);
         for(int j=0; j<i; j++)
             threads[j]->join();
+        times[1] = mysecond();
         PrintResult(cpp_mutex_lock, i);
     }
 
@@ -60,10 +73,12 @@ int main() {
     locktest::LevelhashingSpinlock *levelhashing_spinlock = new locktest::LevelhashingSpinlock();
     for(int i=1; i<=MAX_NR_THREADS; i++) {
         counter = 0;
+        times[0] = mysecond();
         for(int j=0; j<i; j++)
             threads[j] = new std::thread(Add, levelhashing_spinlock, NR_ADD_OPERATIONS/i);
         for(int j=0; j<i; j++)
             threads[j]->join();
+        times[1] = mysecond();
         PrintResult(levelhashing_spinlock, i);
     }
 
--- src/rwlock_test.cc
+++ src/rwlock_test.cc
@@ -16,51 +16,80 @@
 
 #include <thread>
 #include <iostream>
-
-#define MAX_NR_THREADS      4
-#define NR_ADD_OPERATIONS   1000*1000*100
+#include <sys/time.h>
+/*
+ * To test rwlock, this value should be even and must be equal or greater than 2.
+ * We will use the half of MAX_NR_THREADS to run read lock, and
+ * the other half to run read lock.
+ */
+#define MAX_NR_THREADS      8
+#define NR_ADD_OPERATIONS   1000*1000*1
 
 volatile unsigned long counter = 0;
+unsigned long cur_counter = 0;
+double *times;
+
+double mysecond() {
+    struct timeval tp;
+    int i;
+    i = gettimeofday(&tp, NULL);
+    return ( (double)tp.tv_sec + (double)tp.tv_usec*1.e-6);
+}
 
 void Add(locktest::AbstractRwLock *rwlock, int nr_ops) {
+    double sub_time;
+    sub_time = mysecond();
     for(int i=0; i<nr_ops; i++) {
         rwlock->WriteLock();
         counter++;
         rwlock->WriteUnlock();
     }
+    std::cout << "Add: " << mysecond() - sub_time << std::endl;
 }
 
 void Read(locktest::AbstractRwLock *rwlock, int nr_ops) {
+    double sub_time;
+    sub_time = mysecond();
     for(int i=0; i<nr_ops; i++) {
         rwlock->ReadLock();
-        counter++;
+        cur_counter = counter;
         rwlock->ReadUnlock();
     }
+    std::cout << "Read: " << mysecond() - sub_time << std::endl;
 }
 
 void PrintResult(locktest::AbstractRwLock *rwlock, int nr_thread) {
     std::cout << rwlock->GetName();
-    if(NR_ADD_OPERATIONS/nr_thread*nr_thread != counter) {
+    if(NR_ADD_OPERATIONS/(nr_thread/2)*(nr_thread/2) != counter) {
         std::cout << ", error, the counter is: " << counter << \
-        ", but expected: " << NR_ADD_OPERATIONS/nr_thread*nr_thread << \
-        ", thread num: " << nr_thread << std::endl;
+        ", but expected: " << NR_ADD_OPERATIONS/(nr_thread/2)*(nr_thread/2);
         exit(-1);
     }else {
-        std::cout << ", ok" << \
-        ", thread num: " << nr_thread << std::endl;
+        std::cout << ", ok";
     }
+    std::cout << ", thread num: " << nr_thread << \
+    ", total time: " << times[1]-times[0] << \
+    ", the number of add operations: " << NR_ADD_OPERATIONS << std::endl;
 }
 
 int main() {
     std::thread *threads[MAX_NR_THREADS];
+    times = new double [2];
     //test cpp mutex lock
     locktest::CppSharedMutexLock *cpp_shared_mutex_lock = new locktest::CppSharedMutexLock();;
-    for(int i=1; i<=MAX_NR_THREADS; i++) {
+    for(int i=2; i<=MAX_NR_THREADS; i+=2) {
         counter = 0;
-        for(int j=0; j<i; j++)
-            threads[j] = new std::thread(Add, cpp_shared_mutex_lock, NR_ADD_OPERATIONS/i);
-        for(int j=0; j<i; j++)
+        times[0] = mysecond();
+        for(int j=0; j<i; j++) {
+            if(j%2 == 0)
+                threads[j] = new std::thread(Read, cpp_shared_mutex_lock, NR_ADD_OPERATIONS);
+            else
+                threads[j] = new std::thread(Add, cpp_shared_mutex_lock, NR_ADD_OPERATIONS/(i/2));
+        }
+        for(int j=0; j<i; j++) {
             threads[j]->join();
+        }
+        times[1] = mysecond();
         PrintResult(cpp_shared_mutex_lock, i);
     }
     return 0;
